8b0391afb176b360464e50b5608830290ff1fc41
白菜林 <noreply@3ae.cn>
2022-11-23 21:37:59 +0800

    debug

--- _git.sh
+++ _git.sh
@@ -6,3 +6,5 @@ git push -u github master
 git push -u gitee master
 # git remote add gitee1 git@gitee.com:baicai_static/static.git
 git push -u gitee1 master
+# git remote add itcode git@gitcode.net:baicai_static/static.git
+git push -u itcode master
