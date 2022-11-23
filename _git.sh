cd2b09a391ce82e9e5be508f513a252bdc186158
白菜林 <noreply@3ae.cn>
2022-11-23 20:42:29 +0800

    debug

--- _git.sh
+++ _git.sh
@@ -1,18 +1,8 @@
-cd ../static/
-git add .
-git commit -m "debug"
-git push
+#!/bin/bash
 
-cd ../static_router/
-cp -rf ../static/app/* ./app/
-cp -rf ../static/json/* ./json/
-cp -rf ../static/_git.sh ./
-cp -rf ../static/ping ./
-cp -rf ../static/.gitignore ./
-cp -rf ../static/LICENSE ./
-
-cd ../static_router/
 git add .
 git commit -m "debug"
 git push -u github master
 git push -u gitee master
+git remote add gitee1 git@gitee.com:baicai_static/static.git
+git push -u gitee1 master
