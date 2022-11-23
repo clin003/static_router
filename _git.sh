b9d3900ff9fd2573dc755a81187746974b5a80ca
白菜林 <noreply@3ae.cn>
2022-11-23 20:43:25 +0800

    debug

--- _git.sh
+++ _git.sh
@@ -4,5 +4,5 @@ git add .
 git commit -m "debug"
 git push -u github master
 git push -u gitee master
-git remote add gitee1 git@gitee.com:baicai_static/static.git
+# git remote add gitee1 git@gitee.com:baicai_static/static.git
 git push -u gitee1 master
