8288e459a9c0952ac5bbe5f00e714ba6857c4e3b
LYHUILIN <clin003@vip.qq.com>
2022-12-26 22:02:09 +0800

    debug

--- _git.sh
+++ _git.sh
@@ -2,11 +2,11 @@
 
 git add .
 git commit -m "debug"
-# git remote add github git@github.com:clin003/static_router.git  #
+# git remote add github git@github.com:clin003/static_router.git
 git push -u github master
-# git remote add gitee git@gitee.com:lyhuilin/static_router.git  #
+# git remote add gitee git@gitee.com:lyhuilin/static_router.git
 git push -u gitee master
-# git remote add gitee1 git@gitee.com:baicai_static/static.git  #
+# git remote add gitee1 git@gitee.com:baicai_static/static.git
 git push -u gitee1 master
-# git remote add itcode git@gitcode.net:baicai_static/static.git  #
+# git remote add itcode git@gitcode.net:baicai_static/static.git
 git push -u itcode master
